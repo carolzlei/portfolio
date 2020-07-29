cbc401e00279835f66a1ad175f66a77d3f6b95f3
Carol Lei <carollei@Carols-MacBook-Pro.local>
2020-07-29 15:55:23 -0700

    Updates

---
+++ precache-manifest.c7704d98a39a48711fff3dd1e0056677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e91afb3c4cc1890fe502254dfd4232",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c76462f388e42bc1be8c",
+    "url": "/portfolio/static/css/main.ae1ebcf8.chunk.css"
+  },
+  {
+    "revision": "717aaa295844e3916816",
+    "url": "/portfolio/static/js/2.2aea96bc.chunk.js"
+  },
+  {
+    "revision": "c76462f388e42bc1be8c",
+    "url": "/portfolio/static/js/main.a57f5862.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
